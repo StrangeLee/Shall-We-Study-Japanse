f1b6ac4da9662c9ecb6788b3380b0628a664c78f
SangHa,Lee <38284431+StrangeLee@users.noreply.github.com>
2020-12-10 18:08:19 +0900

    [ADD] Util Widget에서 roundedButton 추가
    Util Widget 에서 둥근 모소리를 가진 버튼위젯 추가 및 Text 위젯에 Align 추가

--- lib/base/util_widget.dart
+++ lib/base/util_widget.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:shall_we_study_japanese/base/custom_text_style.dart';
@@ -14,6 +15,47 @@ class UtilWidget {
     return PlatformText(
       text,
       style: textStyle,
+      textAlign: TextAlign.center,
+    );
+  }
+
+  static Widget roundedButton({
+    @required Widget child,
+    @required Function function,
+    @required double radiusValue,
+    EdgeInsetsGeometry margin,
+    EdgeInsetsGeometry padding,
+  }) {
+    return Container(
+      margin: margin,
+      child: FlatButton(
+        child: child,
+        onPressed: function,
+        padding: padding,
+        color: Colors.greenAccent,
+        // color: Colors.lightGreenAccent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(radiusValue)
+          ),
+          side: BorderSide(
+            color: Colors.greenAccent,
+            width: 3.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  static Widget defaultButton({
+    @required Widget child,
+    @required Function function,
+    Color color,
+  }) {
+    return PlatformButton(
+      child: child,
+      onPressed: function,
+      color: color,
     );
   }
 }
