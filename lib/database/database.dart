f73b06c9a22a576040237f5ec38e9fa8cc58ef91
SangHa,Lee <38284431+StrangeLee@users.noreply.github.com>
2020-12-13 00:56:26 +0900

    [ADD] query문 추가
    DataBase클래스에 기본적인 sql문 함수 생성

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shall_we_study_japanese/model/word_note.dart';
 import 'package:sqflite/sqflite.dart';
 
 final String TABLE_NAME = 'word_note';
@@ -33,5 +34,48 @@ class DB {
               'memo TEXT'
               ')');
     }, onUpgrade: (db, oldVersion, newVersion) {});
+  }
+
+  insertWord(WordNote wordNote) async {
+    final db = await database;
+    var res = await db.insert(TABLE_NAME, wordNote.toJson());
+    return res;
+  }
+
+  //Read
+  getWord(int id) async {
+    final db = await database;
+    var res = await db.query(TABLE_NAME, where: 'id = ?', whereArgs: [id]);
+    return res.isNotEmpty ? WordNote.fromJson(res.first) : Null;
+  }
+
+  //Read All
+  Future<List<WordNote>> getAllWords() async {
+    final db = await database;
+    var res = await db.query(TABLE_NAME);
+    List<WordNote> list =
+    res.isNotEmpty ? res.map((c) =>WordNote.fromJson(c)).toList() : [];
+
+    return list;
+  }
+
+  updateWord(WordNote wordNote) async {
+    final db = await database;
+    var res = db.update(TABLE_NAME, wordNote.toJson(),
+        where: 'id = ?', whereArgs: [wordNote.id]);
+    return res;
+  }
+
+  //Delete
+  deleteWord(int id) async {
+    final db = await database;
+    var res = db.delete(TABLE_NAME, where: 'id = ?', whereArgs: [id]);
+    return res;
+  }
+
+  //Delete All
+  deleteAllWordNote() async {
+    final db = await database;
+    db.delete(TABLE_NAME);
   }
 }
