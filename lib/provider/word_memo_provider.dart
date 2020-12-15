daa94088769735652f714a77c9c0c15b7bb14106
SangHa,Lee <38284431+StrangeLee@users.noreply.github.com>
2020-12-16 08:55:21 +0900

    [ADD] word_memo_provider.dart
    WordMemoProvider 추가

---
+++ lib/provider/word_memo_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/foundation.dart';
+import 'package:shall_we_study_japanese/database/database.dart';
+import 'package:shall_we_study_japanese/model/word_note.dart';
+
+class WordMemoProvider with ChangeNotifier {
+
+  List<WordNote> _words = [];
+
+  List<WordNote> get getWords {
+    _fetchWordList();
+    return _words;
+  }
+
+  bool checkNullWords({
+    @required String original,
+    @required String meaning,
+    String memo
+  }) {
+    if (original == '' || meaning == '') {
+      return false;
+    } else {
+      WordNote wordNote = WordNote(
+        originalWord: original,
+        translationWord: meaning,
+        memo: memo,
+      );
+
+      DB().insertWord(wordNote);
+      notifyListeners();
+      return true;
+    }
+  }
+
+  _fetchWordList() async {
+    _words = await DB().getAllWords();
+    notifyListeners();
+  }
+}
