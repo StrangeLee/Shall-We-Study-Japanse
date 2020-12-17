62bf2c47a9eb93c5a1c75580b1537ef1b6f53718
SangHa,Lee <38284431+StrangeLee@users.noreply.github.com>
2020-12-17 22:26:51 +0900

    [ADD] TextStyle 추가
    새로운 TextStyle 추가

--- lib/base/custom_text_style.dart
+++ lib/base/custom_text_style.dart
@@ -7,4 +7,14 @@ class CustomTextStyle {
     fontSize: 20.0,
     fontWeight: FontWeight.normal,
   );
+
+  static TextStyle get quizText => const TextStyle(
+    fontSize: 50.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle get answerText => const TextStyle(
+    fontSize: 25.0,
+    fontWeight: FontWeight.normal,
+  );
 }
