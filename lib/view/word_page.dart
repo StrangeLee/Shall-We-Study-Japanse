0ec878b4774bd43d6187aad5c7d99baf658d9a08
SangHa,Lee <38284431+StrangeLee@users.noreply.github.com>
2020-12-19 23:04:25 +0900

    [FIX] WordPage
    todo 추가 및 List데이터 셔플 후 QuizWidget으로 넘기기

--- lib/view/word_page.dart
+++ lib/view/word_page.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:shall_we_study_japanese/base/base_page.dart';
-import 'package:shall_we_study_japanese/base/util_widget.dart';
+import 'package:shall_we_study_japanese/view/quiz_widget.dart';
 
 class WordPage extends BasePage {
   @override
@@ -10,6 +9,10 @@ class WordPage extends BasePage {
 
 class _WordPageState extends BaseState<WordPage> with BasicPage {
 
+  /// DEC, 29, 2020 TODO 1 : DB의 데이터가 처음에 제대로 호출되지 않는 버그 고치기
+  /// DEC, 29, 2020 TODO 2 : 문제를 풀때 IOS, Android 모두 메인페이지로의 이동을 막거나, 다이얼로그를 띄워 한번 더 확인하게 하기
+
+
   @override
   String appbarTitle() {
     return '단어 외우기';
@@ -17,11 +20,10 @@ class _WordPageState extends BaseState<WordPage> with BasicPage {
 
   @override
   Widget userBody() {
-    print(this.provider.getWords.toString());
+    var list = this.provider.getWords;
+    list.shuffle();
     return Center(
-      child: UtilWidget.textView(
-        text: this.defaultText,
-      ),
+      child: QuizWidget(list: list,)
     );
   }
 }
