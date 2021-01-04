ed30d99dfc596a41a7c8a81bcf75122e56451818
SangHa,Lee <38284431+StrangeLee@users.noreply.github.com>
2021-01-04 16:52:30 +0900

    [FIX] QuizWidget
    QuizWidget에서 mark 인자를 받음으로 추후 Navigator사용시 다음문제로 넘어갈지 혹은 다 풀었는지를 확인 할 수 있도록 함.
    따라서 PageView가 아닌 단순한 Container위젯으로 buildWidget을 바꿈

--- lib/view/quiz_widget.dart
+++ lib/view/quiz_widget.dart
@@ -5,110 +5,101 @@ import 'package:shall_we_study_japanese/base/util_widget.dart';
 import 'package:shall_we_study_japanese/model/word_note.dart';
 import 'package:toast/toast.dart';
 
+// Jan, 4, 2021 Todo : 문제 성공 시 화면 전환 및 완료 페이지 만들기
+
 class QuizWidget extends StatefulWidget {
   final List<WordNote> list;
+  final int mark;
 
-  const QuizWidget({Key key, this.list}) : super(key: key);
+  const QuizWidget({Key key, this.list, this.mark}) : super(key: key);
 
   @override
-  _QuizWidgetState createState() => _QuizWidgetState(list);
+  _QuizWidgetState createState() => _QuizWidgetState(list, mark);
 }
 
 class _QuizWidgetState extends State<QuizWidget> {
   List<WordNote> list;
+  int mark;
 
-  _QuizWidgetState(this.list);
+  _QuizWidgetState(this.list, this.mark);
 
   TextEditingController controller = TextEditingController();
-
-  PageController pageController;
-  double pageScrollPosition = 1;
 
   @override
   void initState() {
     super.initState();
-    pageController = PageController(
-      keepPage: true,
-    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      itemCount: list.length,
-      itemBuilder: (context, index) {
-        var item = list[index];
-
-        return Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 300.0,
-                      decoration: BoxDecoration(
-                        color: Colors.lightGreenAccent,
-                        borderRadius: BorderRadius.circular(20.0),
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 300.0,
+                  decoration: BoxDecoration(
+                    color: Colors.lightGreenAccent,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: UtilWidget.textView(
+                        text: list[this.mark].originalWord,
+                        textStyle: CustomTextStyle.quizText,
                       ),
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: UtilWidget.textView(
-                            text: item.originalWord,
-                            textStyle: CustomTextStyle.quizText,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Container(
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    color: Colors.lightGreenAccent,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Center(
+                    child: UtilWidget.textField(
+                      controller: controller,
+                      hintText: '정답 입력란',
+                      padding: const EdgeInsets.all(8.0),
+                      hintStyle: CustomTextStyle.answerText,
+                      maxLines: 1,
+                      align: TextAlign.center,
+                      contentPadding: const EdgeInsets.all(8.0),
+                      textStyle: CustomTextStyle.answerText,
+                      boxDecoration: BoxDecoration(
+                        color: Colors.transparent,
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Colors.black,
+                            width: 1.0,
                           ),
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                    Container(
-                      height: 150.0,
-                      decoration: BoxDecoration(
-                        color: Colors.lightGreenAccent,
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                      child: Center(
-                        child: UtilWidget.textField(
-                          controller: controller,
-                          hintText: '정답 입력란',
-                          padding: const EdgeInsets.all(8.0),
-                          hintStyle: CustomTextStyle.answerText,
-                          maxLines: 1,
-                          align: TextAlign.center,
-                          contentPadding: const EdgeInsets.all(8.0),
-                          textStyle: CustomTextStyle.answerText,
-                          boxDecoration: BoxDecoration(
-                            color: Colors.transparent,
-                            border: Border(
-                              bottom: BorderSide(
-                                color: Colors.black,
-                                width: 1.0,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 16.0),
-                child: UtilWidget.roundedButton(
-                  child: UtilWidget.textView(text: '정답 확인하기'),
-                  function: () => _isAnswer(item.translationWord, controller.text),
-                  radiusValue: 20.0,
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
-        );
-      },
+          Padding(
+            padding: EdgeInsets.only(bottom: 16.0),
+            child: UtilWidget.roundedButton(
+              child: UtilWidget.textView(text: '정답 확인하기'),
+              function: () => _isAnswer(list[mark].translationWord, controller.text),
+              radiusValue: 20.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -117,12 +108,7 @@ class _QuizWidgetState extends State<QuizWidget> {
       Toast.show('삐빅 오답입니다.', context);
     } else {
       Toast.show('삐빅 정답입니다.', context);
-    }
-  }
 
-  void updatePageState() {
-    setState(() {
-      pageScrollPosition = pageController.position.pixels.abs();
-    });
+    }
   }
 }
