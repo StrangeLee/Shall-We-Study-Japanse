b57ef4107be81c95d8c049d8974ac96d10e5d1f8
SangHa,Lee <38284431+StrangeLee@users.noreply.github.com>
2021-01-06 22:34:16 +0900

    [ADD] BaseDialog
    BaseDialog에 선택지가 있는 다이얼로그 함수 추가.

--- lib/base/base_dialog.dart
+++ lib/base/base_dialog.dart
@@ -28,4 +28,34 @@ class BaseDialog {
       ),
     );
   }
+
+  static void showYesOrNoDialog({
+    @required BuildContext context,
+    @required String title,
+    @required String content,
+    Function function,
+  }) {
+    showPlatformDialog(
+      context: context,
+      builder: (context) => PlatformAlertDialog(
+        title: PlatformText(title),
+        content: PlatformText(content),
+        actions: [
+          PlatformDialogAction(
+            child: PlatformText('취소'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          PlatformDialogAction(
+            child: PlatformText('확인'),
+            onPressed: () {
+              Navigator.pop(context);
+              function();
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
